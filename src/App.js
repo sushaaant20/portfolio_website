da351330b57d37cbae46ae959d7af9e3658bcfa2
sushi200 <pawarsushant531@gmail.com>
2022-06-04 15:38:46 +0530

    added introduction page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import About from './Components/about/About.jsx';
 import Intro from './Components/Introduction/Intro.js';
+
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <About />
     </div>
   );
 }
---
+++ src/Components/about/About.jsx
@@ -0,0 +1,12 @@
+import "./about.css"
+
+const About = () => {
+  return (
+    <div className="about">
+        This is the about section
+      
+    </div>
+  )
+}
+
+export default About
