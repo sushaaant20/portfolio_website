84e343cbda83e592dfffebae0cef54ad7b0c6771
sushi200 <pawarsushant531@gmail.com>
2022-06-04 16:41:26 +0530

    added about

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -2,8 +2,28 @@ import "./about.css"
 
 const About = () => {
   return (
-    <div className="about">
-        This is the about section
+    <div className="a">
+        {/*This is the LEFT side of the about page */}
+        <div className="a-left">
+            <div className="a-card bg"></div>
+            <div className="a-card">
+                <img 
+                src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="a-img" />
+            </div>
+        </div>
+
+
+         {/*This is the RIGHT side of the about page */}
+         <div className="a-right">
+             <h4 className="abt">ABOUT ME</h4>
+             <p className="i-desc">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+          </p>
+
+         </div>
+
       
     </div>
   )
